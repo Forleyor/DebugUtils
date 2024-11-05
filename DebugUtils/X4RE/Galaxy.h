0477923ddda55a3f4d549f7613b841423e2b80a1
Forleyor <Forleyorss@protonmail.com>
2024-11-05 08:17:33 +0000

    Formatting

--- DebugUtils/X4RE/Galaxy.h
+++ DebugUtils/X4RE/Galaxy.h
@@ -3,7 +3,7 @@
 class Galaxy
 {
 public:
-	void* vTable;												//0x0000
-	char pad_0000[88];											//0x0008
-	class Macro* macro;											//0x0060
+    void* vTable;                                               //0x0000
+    char pad_0000[88];                                          //0x0008
+    class Macro* macro;                                         //0x0060
 };
