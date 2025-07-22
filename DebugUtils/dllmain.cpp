9a3bc4c687da3b221ebbc1b309e0450dfdc4b605
Dan <gitkraken.8stsz@simplelogin.com>
2025-07-22 19:24:54 +0100

    Add ExportMap option

--- DebugUtils/dllmain.cpp
+++ DebugUtils/dllmain.cpp
@@ -89,144 +89,199 @@ bool isActiveWindow()
     return false;
 }
 
-void LogErrorAndExit(std::string errorMessage)
+bool WriteMemory(void* address, void* newBytes, int size)
 {
-    logger->error("Exiting due to error: {}", errorMessage);
-    ExitThread(0);
+	DWORD oldProtect;
+
+	if (!VirtualProtect((LPVOID)address, size, PAGE_EXECUTE_READWRITE, &oldProtect))
+		return false;
+
+	if (memcpy_s(address, size, newBytes, size))
+		return false;
+
+	if (!VirtualProtect((LPVOID)address, size, oldProtect, &oldProtect))
+		return false;
+
+	return true;
 }
 
-// Created thread to setup our logic
+bool SetupLogFilter(INIReader& config, void* moduleBase, uintptr_t moduleSize)
+{
+	logger->info("Config option: Disable Logs Filter setting up...");
+
+	std::string excluded = config.Get("ExcludedLogStrings", "string", "");
+	if (excluded.empty())
+	{
+		logger->error("EnableLogsFilter is enabled, but ExcludedLogStrings section contains no strings to filter!");
+		return false;
+	}
+
+	excludes = splitKeys(excluded);
+	if (excludes.empty())
+	{
+		logger->error("EnableLogsFilter is enabled, but ExcludedLogStrings section contains no strings to filter!");
+		return false;
+	}
+
+	uintptr_t loggerAddress = (uintptr_t)scan_idastyle(moduleBase, moduleSize, "48 89 5C 24 18 55 56 41 54 41 55 41 57");
+	if (!loggerAddress)
+	{
+		logger->error("Logger address: NULL!");
+		return false;
+	}
+
+	if (MH_CreateHook((void*)loggerAddress, (void*)hookLogger, reinterpret_cast<void**>(&origLogger)) != MH_OK || MH_EnableHook((void*)loggerAddress) != MH_OK)
+	{
+		logger->error("Failed to hook logger, Exiting...");
+		return false;
+	}
+
+    return true;
+}
+
+bool SetupEnableMapEditorExport(void* moduleBase, uintptr_t moduleSize)
+{
+	uintptr_t exportMapAddress = (uintptr_t)scan_idastyle(moduleBase, moduleSize, "0F 84 ? ? ? ? 48 C7 44 24 ? ? ? ? ? 48 C7 44 24 ? ? ? ? ? 48 C7 44 24 ? ? ? ? ? 0F 1F 40");
+
+    if (!exportMapAddress)
+    {
+		logger->error("Failed to get address required for Export Map patch!");
+        return false;
+    }
+
+	char newBytes[] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
+    if (!WriteMemory((void*)exportMapAddress, (void*)newBytes, sizeof(newBytes)))
+    {
+        logger->error("Failed to patch ExportMap, map exporting will not work!");
+        return false;
+    }
+
+    return true;
+}
+
+bool SetupReloadUIHotkey(INIReader& config)
+{
+	Sleep(5 * 1000);
+
+	logger->info("Config option: Reload UI hotkey setting up...");
+
+	getProcID(L"X4", x4Id);
+
+	_GetUIScaleFactor = (__GetUIScaleFactor)GetProcAddress(GetModuleHandle(nullptr), "GetUIScaleFactor");
+	_SetUIScaleFactor = (__SetUIScaleFactor)GetProcAddress(GetModuleHandle(nullptr), "SetUIScaleFactor");
+
+    if (!_GetUIScaleFactor || !_SetUIScaleFactor)
+    {
+		logger->error("Failed to resolve function pointers during Reload UI hotkey setup, /reloadui hotkey will not work.");
+        return false;
+    }
+
+	logger->info("GetUIScaleFactor address: 0x{0:x}", (uintptr_t)_GetUIScaleFactor);
+	logger->info("SetUIScaleFactor address: 0x{0:x}", (uintptr_t)_SetUIScaleFactor);
+
+	UINT reloadUIKeyCode = (UINT)config.GetInteger("Hotkeys", "reloadUI", 0x24);
+
+	bool requireALT = config.GetBoolean("HotkeyModifiers", "requireALT", false);
+	bool requireCTRL = config.GetBoolean("HotkeyModifiers", "requireCTRL", false);
+	bool requireSHIFT = config.GetBoolean("HotkeyModifiers", "requireSHIFT", false);
+
+	UINT modifiers = MOD_NOREPEAT;
+
+	if (requireALT)
+		modifiers |= MOD_ALT;
+	if (requireCTRL)
+		modifiers |= MOD_CONTROL;
+	if (requireSHIFT)
+		modifiers |= MOD_SHIFT;
+
+    if (!RegisterHotKey(nullptr, 1, modifiers, reloadUIKeyCode))
+    {
+		logger->error("Failed to register hotkey: Reload UI. Try another key in the ini file.");
+        return false;
+    }
+
+	logger->info("Registered hotkey: Reload UI.");
+	logger->info("Config option: Reload UI hotkey finished setup successfully");
+	logger->info("Hotkeys ready!");
+
+	MSG msg = { nullptr };
+	while (GetMessage(&msg, nullptr, 0, 0) != 0)
+	{
+		if (msg.message == WM_HOTKEY)
+		{
+			if (isActiveWindow())
+			{
+				logger->info("Reloading UI!");
+				_SetUIScaleFactor(_GetUIScaleFactor());
+			}
+		}
+	}
+
+    return true;
+}
+
 DWORD WINAPI setup(LPVOID param)
 {
     // Init spdlog
     std::filesystem::path logPath = std::filesystem::current_path() / "plugins" / "DebugUtils" / "DebugUtils.log";
     logger = spdlog::basic_logger_mt("DebugUtils", logPath.string(), true);
 
-    // Set spdlog to flush messages to log file on info level
     logger->flush_on(spdlog::level::info);
 
-    // Get the current working directory, this returns x4 root directory
+    // Init Config
     std::filesystem::path iniPath = std::filesystem::current_path() / "plugins" / "DebugUtils" / "DebugUtils.ini";
 
     INIReader config(iniPath.string());
 
-    // Check if config exists
     if (config.ParseError() < 0)
-        LogErrorAndExit("X4Bypass.ini not found, Path was " + iniPath.string());
+    {
+        logger->error("X4Bypass.ini not found, Path was " + iniPath.string());
+        return 0;
+    }
 
-    // Get ini settings from [Config] section
     bool enableLogsFilter = config.GetBoolean("Config", "EnableLogsFilter", false);
     bool enableReloadHotkey = config.GetBoolean("Config", "EnableHotkeys", false);
+    bool enableMapEditorExport = config.GetBoolean("Config", "EnableMapEditorExport", false);
 
     if (!enableLogsFilter && !enableReloadHotkey)
-        LogErrorAndExit("No config options enabled in DebugUtils.ini! Exiting...");
+    {
+        logger->error("No config options enabled in DebugUtils.ini! Exiting...");
+        return 0;
+    }
 
-    // Initialize MinHook
     if (MH_Initialize() != MH_OK)
-        LogErrorAndExit("Minhook initialize failed");
+    {
+        logger->error("Minhook initialize failed");
+        return 0;
+    }
 
-    // Get module info for X4.exe, used later in pattern scanning passing X4 base address and size of module
     MODULEINFO info;
     BOOL result = GetModuleInformation(GetCurrentProcess(), GetModuleHandle(nullptr), &info, sizeof(info));
 
     if (!result)
-        LogErrorAndExit("Failed to get module info for X4.exe");
+    {
+        logger->error("Failed to get module info for X4.exe, exiting.");
+        return 0;
+    }
 
     logger->info("X4 base address: 0x{0:x}", (uintptr_t)info.lpBaseOfDll);
 
     if (enableLogsFilter)
     {
-        logger->info("Config option: Disable Logs Filter setting up...");
-
-        std::string excluded = config.Get("ExcludedLogStrings", "string", "");
-
-        if (excluded.empty())
-            LogErrorAndExit("EnableLogsFilter is enabled, but ExcludedLogStrings section contains no strings to filter!");
-
-        // Split the strings in ExcludedLogStrings into a vector of strings
-        excludes = splitKeys(excluded);
-
-        // Check if there are actually any excluded strings
-        if (excludes.empty())
-        {
-            LogErrorAndExit("EnableLogsFilter is enabled, but ExcludedLogStrings section contains no strings to filter!");
-            return 0;
-        }
-
-        // Get the address of X4.exe Debug log logging function
-        uintptr_t loggerAddress = (uintptr_t)scan_idastyle(info.lpBaseOfDll, info.SizeOfImage, "48 89 5C 24 18 55 56 41 54 41 55 41 57");
-
-        if (!loggerAddress)
-            LogErrorAndExit("Logger address: NULL!");
-
-        // Create the hook in MinHook and enable it
-        if (MH_CreateHook((void*)loggerAddress, (void*)hookLogger, reinterpret_cast<void**>(&origLogger)) != MH_OK || MH_EnableHook((void*)loggerAddress) != MH_OK)
-            LogErrorAndExit("Failed to hook logger, Exiting...");
-
-        logger->info("Config option: Disable Logs Filter finished setup successfully");
+        if (SetupLogFilter(config, info.lpBaseOfDll, info.SizeOfImage))
+			logger->info("Config option: Disable Logs Filter finished setup successfully");
     }
+
+	if (enableMapEditorExport)
+	{
+        if (SetupEnableMapEditorExport(info.lpBaseOfDll, info.SizeOfImage))
+        logger->info("ExportMap patch success");
+	}
 
     if (enableReloadHotkey)
     {
-        // give the game time to load a bit
-        Sleep(5 * 1000);
-
-        logger->info("Config option: Reload UI hotkey setting up...");
-
-        // Get process ID for X4
-        // used in isActiveWindow() so hotkey doesnt trigger when X4 is out of focus
-        getProcID(L"X4", x4Id);
-
-        // get the address of X4.exe function handles /commands
-        _GetUIScaleFactor = (__GetUIScaleFactor)scan_idastyle(info.lpBaseOfDll, info.SizeOfImage, "8B 05 ? ? ? ? 0F 57 C0 F3 0F 10 0D ? ? ? ? F3 0F 10 15");
-        _SetUIScaleFactor = (__SetUIScaleFactor)scan_idastyle(info.lpBaseOfDll, info.SizeOfImage, "48 83 EC 48 0F 57 C9");
-
-        if (!_GetUIScaleFactor || !_SetUIScaleFactor)
-            LogErrorAndExit("Failed to resolve function pointers during Reload UI hotkey setup!");
-
-		logger->info("GetUIScaleFactor address: 0x{0:x}", (uintptr_t)_GetUIScaleFactor);
-		logger->info("SetUIScaleFactor address: 0x{0:x}", (uintptr_t)_SetUIScaleFactor);
-
-        // Get the user defined keycode from ini file, defaults to VK_HOME (0x24) if not set in ini file
-        UINT reloadUIKeyCode = (UINT)config.GetInteger("Hotkeys", "reloadUI", 0x24);
-
-        bool requireALT = (UINT)config.GetBoolean("HotkeyModifiers", "requireALT", false);
-        bool requireCTRL = (UINT)config.GetBoolean("HotkeyModifiers", "requireCTRL", false);
-        bool requireSHIFT = (UINT)config.GetBoolean("HotkeyModifiers", "requireSHIFT", false);
-
-        UINT modifiers = MOD_NOREPEAT;
-
-        if (requireALT)
-            modifiers |= MOD_ALT;
-        if (requireCTRL)
-            modifiers |= MOD_CONTROL;
-        if (requireSHIFT)
-            modifiers |= MOD_SHIFT;
-
-        // Register hotkey for reloading UI
-        if (!RegisterHotKey(nullptr, 1, modifiers, reloadUIKeyCode))
-            LogErrorAndExit("Failed to register hotkey: Reload UI. Try another key in the ini file.");
-
-        logger->info("Registered hotkey: Reload UI.");
-        logger->info("Config option: Reload UI hotkey finished setup successfully");
-        logger->info("Hotkeys ready!");
-
-        // When hotkey pressed, reload UI
-        MSG msg = { nullptr };
-        while (GetMessage(&msg, nullptr, 0, 0) != 0)
-        {
-            if (msg.message == WM_HOTKEY)
-            {
-                if (isActiveWindow())
-                {
-                    logger->info("Reloading UI!");
-                    _SetUIScaleFactor(_GetUIScaleFactor());
-                }
-            }
-        }
+        SetupReloadUIHotkey(config);
     }
-
-    std::string test;
 
     return 0;
 }
